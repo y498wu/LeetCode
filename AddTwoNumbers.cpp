86f8a64283ff8e601695f28584300ff164384456
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-07 16:12:05 -0400

    My own version

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -13,7 +13,46 @@ public:
         ListNode* p = l1;
         ListNode* q = l2;
         ListNode* curr = dummyHead;
+        int carry = 0;/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dumbHead = new ListNode{0};
+        ListNode* cur1 = l1;
+        ListNode* cur2 = l2;
+        ListNode* curr = dumbHead;
         int carry = 0;
+        int result = 0;
+        while(cur1 != nullptr || cur2 != nullptr){
+            if (cur1 == nullptr){
+                result = cur2->val;
+            } else if (cur2 == nullptr){
+                result = cur1->val;
+            } else {
+                result = cur1->val + cur2->val;
+            }
+            if (carry == 1) result++;
+            if (result >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            curr->next = new ListNode{result%10};
+            if(cur1 != nullptr) cur1 = cur1->next;
+            if(cur2 != nullptr) cur2 = cur2->next;
+            curr = curr->next;
+        }
+        if (carry == 1) curr->next = new ListNode{1};
+        return dumbHead->next;
+    }
+};
         int x, y;
         while (p || q){
             (p != nullptr) ? x = p->val : x = 0;
