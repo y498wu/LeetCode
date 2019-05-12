ddbff988ab45da6e1d1161de4feaa97fae91e2b9
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-12 18:07:33 -0400

    Basic tests pass, but somehow get wrong answer
    Failed at test case:
    Input: ""
    Output: 1
    Expected: 0

--- Q3.LongestSubstring.cpp
+++ Q3.LongestSubstring.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
+    void printVec(vector<char> &vec){
+        for(int i = 0; i < vec.size(); ++i){
+            cout << vec.at(i);
+        }
+        cout << endl;
+    }
     int lengthOfLongestSubstring(string s) {
-        vector<char> cur = {s[0]};
-        for(char& origi : s){
-            for(char curChar : cur){
-                if (origi == curChar){
-                    break;
-                } else {
-                    cur.emplace_back(origi);
+        // store the current maximum substring in vector
+        vector<char> cur;
+        // store the possible substring to be compared with cur
+        vector<char> curCompare;
+        for(int i = 0; i < s.length(); i++){
+            // compare lenCompare and len
+            printVec(cur);
+            printVec(curCompare);
+            cout << cur.size() << " " << curCompare.size() << endl;
+            if(curCompare.size() > cur.size()){
+                cur.clear();
+                cur = curCompare;
+            }
+            if(curCompare.empty()){
+                curCompare.emplace_back(s[i]);
+            } else {
+                for(int j = 0; j < cur.size(); ++j){
+                    if(s[i] == cur[j]){
+                        curCompare.clear();
+                    }
                 }
+                curCompare.emplace_back(s[i]);
             }
         }
         return cur.size();
