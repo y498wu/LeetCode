b8b44aa2811765ca414b134efd53bbab94f44d2e
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-14 10:21:20 -0400

    Pass 493 / 987 test cases but fail cuz time limit

--- 003.LongestSubstring.cpp
+++ 003.LongestSubstring.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    void printVec(vector<char> &vec){
-        for(int i = 0; i < vec.size(); ++i){
-            cout << vec.at(i);
+    bool isUnique(string s, int lower, int upper){
+        // for(int k = lower; k <= upper; ++k){
+        //     cout << s[k];
+        // }
+        // cout << endl;
+        if (lower == upper) return true;
+        for(int i = lower; i < upper; ++i){
+            for(int j = i+1; j <= upper; ++j){
+                // cout << i << j << endl;
+                if(s[i] == s[j]) return false;
+            }
         }
-        cout << endl;
+        return true;
     }
     int lengthOfLongestSubstring(string s) {
-        // store the current maximum substring in vector
-        vector<char> cur;
-        // store the possible substring to be compared with cur
-        vector<char> curCompare;
-        int len = 0;
-        for(int k = 0; k < s.length(); k++){
-        for(int i = k; i < s.length(); i++){
-            if(curCompare.empty()){
-                curCompare.emplace_back(s[i]);
-            } else {
-                for(int j = 0; j < cur.size(); ++j){
-                    if(s[i] == curCompare[j]){
-                        curCompare.clear();
-                    }
-                }
-                curCompare.emplace_back(s[i]);
+        int ans = 0;
+        int len = s.length();
+        if(len == 1) return 1;
+        for(int i = 0; i < len-1; ++i){
+            for(int j = i; j < len; ++j){
+                // cout << isUnique(s, i, j) << s[i] << s[j] << j-i << endl; 
+                // cout << i << j << endl;
+                if(isUnique(s, i, j)) ans = max(ans, j-i+1);
             }
-            // compare lenCompare and len
-            if(curCompare.size() >= cur.size()){
-                cur.clear();
-                cur = curCompare;
-            }
-            //printVec(cur);
-            //printVec(curCompare);
-            //cout << cur.size() << " " << curCompare.size() << endl;
         }
-            if(cur.size() > len) len = cur.size();
-            cur.clear();
-            curCompare.clear();
-        }
-        return len;
+        return ans;
     }
 };
