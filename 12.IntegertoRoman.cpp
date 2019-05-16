6abafa9888e0bdadcc277b65d3d5a1491336063a
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-16 11:17:26 -0400

    Initial solution with compiling error

---
+++ 12.IntegertoRoman.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        if(num == 0) return "";
+        vector<string> ans;
+        int pos = 1;
+        string one = "I";
+        string five = "V";
+        string ten = "X";
+        while(num != 0){
+            if(pos == 2){
+                one = "X";
+                five = "L";
+                ten == "C";
+            } else if (pos == 3){
+                one = "C";
+                five = "D";
+                ten == "M";
+            } else if (pos == 4){
+                one = "M";
+            }
+            int digit = num%10;
+            string oneDigit = "";
+            if(digit == 1) oneDigit = one;
+            if(digit == 2) oneDigit = one + one;
+            if(digit == 3) oneDigit = one + one + one;
+            if(digit == 4) oneDigit = one + five;
+            if(digit == 5) oneDigit = five;
+            if(digit == 6) oneDigit = five + one;
+            if(digit == 7) oneDigit = five + one + one;
+            if(digit == 8) oneDigit = five + one + one + one;
+            if(digit == 9) oneDigit = one + ten;
+            ans.emplace_back(oneDigit);
+            num /= 10;
+            ++pos;
+        } 
+        int len = ans.size();
+        string ansStr = "";
+        for(int i = 0; i < len; ++i){
+            ansStr.append(ans.pop_back());
+        }
+        return ansStr;
+    }
+};
