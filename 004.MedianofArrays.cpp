dacd3795fe040813783a661bdac9601988c82454
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-18 19:43:41 -0400

    merge two vectors into one

--- 004.MedianofArrays.cpp
+++ 004.MedianofArrays.cpp
@@ -1,90 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size();
-        int len2 = nums2.size();
-        bool isOdd;
-        if((len1+len2)%2 == 0){
-            isOdd = false;
-        } else {
-            isOdd = true; 
-        }
-        int medianPos = (len1+len2+1)/2;
-        // check the case when nums1 or nums2 is null
-        if(len1 == 0 && isOdd){
-            return nums2[medianPos-1];
-        } else if (len1 == 0 && !isOdd){
-            return (double(nums2[medianPos-1]) + double(nums2[medianPos])) / 2;
-        } else if (len2 == 0 && isOdd){
-            return nums1[medianPos-1];
-        } else if (len2 == 0 && !isOdd){
-            return (double(nums1[medianPos-1]) + double(nums1[medianPos])) / 2;
-        }
-        // 1 if point to nums1
-        // 2 if point to nums2
-        int sideha = 1;
-        int sidehaha = 2;
-        // pos of ha and haha
-        int posha = 0;
-        int poshaha = 0;
-        int count = 1;
-        while(1){
-            // assign values to valha and valhaha
-            int valha, valhaha;
-            if (sideha == 1){
-                valha = nums1[posha];
-            } else if (sideha == 2){
-                valha = nums2[posha];
-            }
-            if (sidehaha == 1){
-                valhaha = nums1[poshaha];
-            } else if (sidehaha == 2){
-                valhaha = nums2[poshaha];
-            }
-            cout << valha << valhaha << endl;
-            // return the correct answer
-            if (count == medianPos && isOdd){
-                return min(double(valha), double(valhaha));
-            } else if (count == medianPos && !isOdd){
-                return (double(valha) + double(valhaha)) / 2;
-            }
-            ++count;
-            // compare valha and valhaha
-            if (valha <= valhaha){
-                if (sideha == 1 && posha == len1-1){
-                    sideha = 2;
-                    posha = poshaha+1;
-                } else if (sideha == 2 && posha == len2-1){
-                    sideha = 1;
-                    posha = poshaha+1;
-                } else if (sidehaha == 1 && poshaha == len1-1){
-                    sideha = 2;
-                    posha = posha+1;
-                } else if (sidehaha == 2 && poshaha == len2-1){
-                    sideha = 1;
-                    posha = posha+1;
-                } else {
-                    sideha = sidehaha;
-                    posha = poshaha+1;
-                }
-            } else {
-                if (sidehaha == 1 && poshaha == len1-1){
-                    sidehaha = 2;
-                    poshaha = posha+1;
-                } else if (sidehaha == 2 && poshaha == len2-1){
-                    sidehaha = 1;
-                    poshaha = posha+1;
-                } else if (sideha == 1 && posha == len1-1){
-                    sidehaha = 2;
-                    poshaha = poshaha+1;
-                } else if (sideha == 2 && posha == len2-1){
-                    sidehaha = 1;
-                    poshaha = poshaha+1;
-                } else {
-                    sidehaha = sideha;
-                    poshaha = posha+1;
-                }
-            }
-        }
-    }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> ans;
+    int i = 0, j = 0, k = 0; 
+  
+    // Traverse both array 
+    while (i<n1 && j <n2) 
+    { 
+        // Check if current element of first 
+        // array is smaller than current element 
+        // of second array. If yes, store first 
+        // array element and increment first array 
+        // index. Otherwise do same with second array 
+        if (nums1[i] < nums2[j]) 
+            ans.emplace_back(nums1[i++]); 
+        else
+            ans.emplace_back(nums2[j++]); 
+    } 
+  
+    // Store remaining elements of first array 
+    while (i < n1) 
+        ans.emplace_back(nums1[i++]); 
+  
+    // Store remaining elements of second array 
+    while (j < n2) 
+        ans.emplace_back(nums2[j++]);
+    
+    int ansSize = ans.size();
+    if (ansSize%2 == 0){
+        return (double(ans[ansSize/2]) + double(ans[ansSize/2 - 1])) /2;
+    } else {
+        return double(ans[ansSize/2]);
+    }}
 };
