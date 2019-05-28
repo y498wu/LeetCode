217778d8c01f1b73d4b36afd084e8098df868d9b
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-28 13:25:34 -0400

    My optimized version with comments

--- 002.AddTwoNumbers.cpp
+++ 002.AddTwoNumbers.cpp
@@ -9,64 +9,44 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode{0};
-        ListNode* p = l1;
-        ListNode* q = l2;
-        ListNode* curr = dummyHead;
-        int carry = 0;/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // create new ListNode dumbHead starting with 0
         ListNode* dumbHead = new ListNode{0};
-        ListNode* cur1 = l1;
-        ListNode* cur2 = l2;
+        // create three pointers to l1, l2, and dumbHead
+        ListNode* p1 = l1;
+        ListNode* p2 = l2;
         ListNode* curr = dumbHead;
+        // create two integers meaning the sum and carry
+        int sum = 0;
         int carry = 0;
-        int result = 0;
-        while(cur1 != nullptr || cur2 != nullptr){
-            if (cur1 == nullptr){
-                result = cur2->val;
-            } else if (cur2 == nullptr){
-                result = cur1->val;
+        // iterate through l1 and l2
+        while(p1 != nullptr || p2 != nullptr){
+            // only add the non-null node's value to sum if the other node is empty
+            if (p1 == nullptr){
+                sum = p2->val + carry;
+            } else if (p2 == nullptr){
+                sum = p1->val + carry;
             } else {
-                result = cur1->val + cur2->val;
+                sum = p1->val + p2->val + carry;
             }
-            if (carry == 1) result++;
-            if (result >= 10) {
+            // get new carry
+            if(sum >= 10){
                 carry = 1;
             } else {
                 carry = 0;
             }
-            curr->next = new ListNode{result%10};
-            if(cur1 != nullptr) cur1 = cur1->next;
-            if(cur2 != nullptr) cur2 = cur2->next;
+            // create a new node containing sum%10
+            curr->next = new ListNode{sum%10};
+            // p1, p2, curr go to next node if not null
+            if(p1 != nullptr){
+                p1 = p1->next;
+            }
+            if (p2 != nullptr){
+                p2 = p2->next;
+            }
             curr = curr->next;
         }
-        if (carry == 1) curr->next = new ListNode{1};
+        // if carry is equal to end after the loop, create a new node with value 1
+        if(carry == 1) curr->next = new ListNode{1};
         return dumbHead->next;
-    }
-};
-        int x, y;
-        while (p || q){
-            (p != nullptr) ? x = p->val : x = 0;
-            (q != nullptr) ? y = q->val : y = 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-            if (p != nullptr) p = p->next;
-            if (q != nullptr) q = q->next;
-        }
-        if (carry > 0) {
-            curr->next = new ListNode(carry);
-        }
-        return dummyHead->next;
     }
 };
