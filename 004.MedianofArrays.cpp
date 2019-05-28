f1abb7edb73bb963114b83214d2ec39ee2d7088a
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-28 15:56:41 -0400

    My same algorithm with comments

--- 004.MedianofArrays.cpp
+++ 004.MedianofArrays.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        int i = 0;
+        int j = 0;
+        // create ans vector
         vector<int> ans;
-    int i = 0, j = 0, k = 0; 
-  
-    // Traverse both array 
-    while (i<n1 && j <n2) 
-    { 
-        // Check if current element of first 
-        // array is smaller than current element 
-        // of second array. If yes, store first 
-        // array element and increment first array 
-        // index. Otherwise do same with second array 
-        if (nums1[i] < nums2[j]) 
-            ans.emplace_back(nums1[i++]); 
-        else
-            ans.emplace_back(nums2[j++]); 
-    } 
-  
-    // Store remaining elements of first array 
-    while (i < n1) 
-        ans.emplace_back(nums1[i++]); 
-  
-    // Store remaining elements of second array 
-    while (j < n2) 
-        ans.emplace_back(nums2[j++]);
-    
-    int ansSize = ans.size();
-    if (ansSize%2 == 0){
-        return (double(ans[ansSize/2]) + double(ans[ansSize/2 - 1])) /2;
-    } else {
-        return double(ans[ansSize/2]);
-    }}
+        while(i < len1 && j < len2){
+            // compare nums1[i] and nums2[j]
+            // push the smaller one to the ans vector
+            if(nums1[i] < nums2[j]){
+                ans.emplace_back(nums1[i]);
+                ++i;
+            } else {
+                ans.emplace_back(nums2[j]);
+                ++j;
+            }
+        }
+        // add the rest of nums1 or nums2 to ans vector
+        while(i < len1){
+            ans.emplace_back(nums1[i]);
+            ++i;
+        }
+        while(j < len2){
+            ans.emplace_back(nums2[j]);
+            ++j;
+        }
+        // get the median depending on if the length of ans vector is odd
+        int anslen = ans.size();
+        if(anslen%2 == 0){
+            return (double(ans[anslen/2]) + double(ans[anslen/2-1]))/2;
+        } else {
+            return double(ans[anslen/2]);
+        }
+        
+    }
 };
