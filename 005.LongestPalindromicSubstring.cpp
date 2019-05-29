ea9d1d0bd76d6d9451adbbdb72631a59b9001c65
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-29 16:25:51 -0400

    My O(n) version faster than 99.61% with comments

--- 005.LongestPalindromicSubstring.cpp
+++ 005.LongestPalindromicSubstring.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int start = 0, end = 0;
-        // iterate through string s
         int slen = s.length();
-        if(slen < 1) return "";
-        int lensolo, lenboth;
-        for(int i = 0; i < slen; ++i){
-            lensolo = expandfromCenter(s, i, i);
-            lenboth = expandfromCenter(s, i, i+1);
-            // check which one is larger
-            if(max(lensolo, lenboth) < end-start+1) continue;
-            if(lensolo > lenboth){
-                start = i-lensolo/2;
-                end = i+lensolo/2;
-            } else {
-                start = i-lenboth/2+1;
-                end = i+lenboth/2;
+        int max_s = 0, max_l = 0;
+        int start = 0, end = 0;
+        for(int i = 0; i < slen;){
+            // make start and end equal to i
+            start = i, end = i;
+            // check if the core centre are same characters
+            while(end+1 < slen && s[end]==s[end+1]){
+                ++end;
+            }
+            i = end+1;
+            // expand from centre if applicable
+            while(start-1 >= 0 && end+1 < slen && s[start-1] == s[end+1]){
+                --start;
+                ++end;
+            }
+            // compare current length to max_l
+            if(end-start+1 > max_l){
+                max_l = end-start+1;
+                max_s = start;
             }
         }
-        return s.substr(start, end-start+1);
-    }
-    
-private:
-    int expandfromCenter(string s, int left, int right){
-        int len = s.length();
-        // check if left and right index are in the range of s
-        // and if s[left] is the same as s[right]
-        while(left >= 0 && right < len && s[left]==s[right]){
-            --left;
-            ++right;
-        }
-        // return the length between left and right (both chars included)
-        return right-left-1;
+        return s.substr(max_s, max_l);
     }
 };
