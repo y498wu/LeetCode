201b40516a09b9a1a5e2a296965a7b73a2dc50c3
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-29 11:24:55 -0400

    My O(n^2) version faster than 60%

--- 005.LongestPalindromicSubstring.cpp
+++ 005.LongestPalindromicSubstring.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    bool isValid(string& s, int start, int end){
-        int half = (end-start)/2;
-        for(int i = 0; i <= half; ++i){
-            if(s[start+i] != s[end-i]) return false;
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
-        int len = s.length();
-        if (len == 1) return s;
-        string ans;
-        for(int i = 0; i < len-1; ++i){
-            for(int j = i; j < len; ++j){
-                string tmp = s.substr(i, (j-i+1));
-                if(isValid(s, i, j) && tmp.length() > ans.length()){
-                    cout << i << j;
-                    ans = tmp;
-                }
+        int start = 0, end = 0;
+        // iterate through string s
+        int slen = s.length();
+        if(slen < 1) return "";
+        int lensolo, lenboth;
+        for(int i = 0; i < slen; ++i){
+            lensolo = expandfromCenter(s, i, i);
+            lenboth = expandfromCenter(s, i, i+1);
+            // check which one is larger
+            if(max(lensolo, lenboth) < end-start+1) continue;
+            if(lensolo > lenboth){
+                start = i-lensolo/2;
+                end = i+lensolo/2;
+            } else {
+                start = i-lenboth/2+1;
+                end = i+lenboth/2;
             }
         }
-        return ans;
+        return s.substr(start, end-start+1);
+    }
+    
+private:
+    int expandfromCenter(string s, int left, int right){
+        int len = s.length();
+        // check if left and right index are in the range of s
+        // and if s[left] is the same as s[right]
+        while(left >= 0 && right < len && s[left]==s[right]){
+            --left;
+            ++right;
+        }
+        // return the length between left and right (both chars included)
+        return right-left-1;
     }
 };
