0e1abe98fb64963d55ea98dc32180d45d0e3f432
y498wu <49050856+y498wu@users.noreply.github.com>
2019-05-30 10:05:43 -0400

    My mapping solution with comments

--- 013. RomantoInteger.cpp
+++ 013. RomantoInteger.cpp
@@ -1,15 +1,27 @@
-//Only when a smaller roman numeral appears before a bigger roman numeral (an inverse) in the string, the smaller one should be deducted.   
-
 class Solution {
 public:
     int romanToInt(string s) {
+        // create a char dictionary
+        vector<int> dict (256, 0);
+        dict['I'] = 1;
+        dict['V'] = 5;
+        dict['X'] = 10;
+        dict['L'] = 50;
+        dict['C'] = 100;
+        dict['D']= 500;
+        dict['M']= 1000;
+        // iterate through string s
+        int slen = s.length();
         int ans = 0;
-        int len = s.length();
-        int map[256];
-        map['I']=1;map['V']=5;map['X']=10;map['L']=50;map['C']=100;map['D']=500;map['M']=1000;
-        for(int i=0;i<len;i++){
-            if(i+1<len && map[s[i]]<map[s[i+1]]) ans-=map[s[i]];
-            else ans+=map[s[i]];
+        for(int i = 0; i < slen; ++i){
+            // check if we get combo like IV or IX
+            if(i+1 < slen && (dict[s[i+1]] == 5*dict[s[i]] || dict[s[i+1]] == 10*dict[s[i]])){
+                ans += dict[s[i+1]];
+                ans -= dict[s[i]];
+                i++;
+            } else {
+                ans += dict[s[i]];
+            }
         }
         return ans;
     }
