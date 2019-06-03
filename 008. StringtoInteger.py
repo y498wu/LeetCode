d670caa323814a005e152e38bfcb55b8c8204f3a
y498wu <49050856+y498wu@users.noreply.github.com>
2019-06-03 16:54:09 -0400

    My own version faster than 86.05%

--- 008. StringtoInteger.py
+++ 008. StringtoInteger.py
@@ -4,13 +4,18 @@ public:
         int slen = str.length();
         int ans = 0;
         bool isPos = true;
+        bool charOnly = false;
         for(int i = 0; i < slen; ++i){
             if(isPos == false && ans > 0) ans = 0 - ans;
             char c = str[i];
             int digit = (int)c - (int)'0';
-            if(c == ' ' || (c == '+' && i+1 < slen && str[i+1] >= '0' && str[i+1] <= '9')){
+            if(charOnly && !(digit >= 0 && digit <= 9)) break;
+            if(c == ' '){
                 continue;
-            } else if (c == '-' && i+1 < slen && str[i+1] >= '0' && str[i+1] <= '9'){
+            } else if (c == '+'){
+                charOnly = true;
+            } else if (c == '-'){
+                charOnly = true;
                 isPos = false;
             } else if (digit >= 0 && digit <= 9){
                 if(ans > INT_MAX/10 || (ans == INT_MAX/10 && digit > 7)) return INT_MAX;
@@ -20,6 +25,7 @@ public:
                 } else {
                     ans = ans*10 + digit;
                 }
+                charOnly = true;
             } else {
                 break;
             }
