6544c97c2e143932d8cad6ad3ece1df1581506cb
y498wu <49050856+y498wu@users.noreply.github.com>
2019-06-19 17:39:19 -0400

    DP solution

--- 070.ClimbingStairs.cpp
+++ 070.ClimbingStairs.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
+    unordered_map<int, int> dict;
     int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        return climbStairs(n-1) + climbStairs(n-2);
+        int f = 0;
+        if(dict.find(n) != dict.end()) return dict[n];
+        if(n == 1){
+            f = 1;
+        } else if(n == 2){
+            f = 2;
+        } else {
+            f = climbStairs(n-1) + climbStairs(n-2);
+        }
+        dict[n] = f;
+        return f;
     }
 };
