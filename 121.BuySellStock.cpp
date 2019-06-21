c842ec9df89e62a2b38dbfb4ed7ac780b26448c1
y498wu <49050856+y498wu@users.noreply.github.com>
2019-06-21 15:01:17 -0400

    Cleaner bottom-up solution

--- 121.BuySellStock.cpp
+++ 121.BuySellStock.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if(prices.empty()) return 0;
-        int ans = 0, start = prices[0];
+        int ans = 0, start = INT_MAX;
         int len = prices.size();
         for(int i = 0; i < len; ++i){
-            int sub = prices[i]-start;
-            if(prices[i] < start){
-                start = prices[i];
-            } else {
-                ans = max(ans, sub);
-            }
+            ans = max(ans, prices[i]-start);
+            start = min(start, prices[i]);
         }
         return ans;
     }
