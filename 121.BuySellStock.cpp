2145d6c13d792525bd2923a07476c4bd36117672
y498wu <49050856+y498wu@users.noreply.github.com>
2019-06-21 14:56:18 -0400

    My 4ms iteration solution

--- 121.BuySellStock.cpp
+++ 121.BuySellStock.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+        int ans = 0, start = prices[0];
         int len = prices.size();
-        int max_sub = 0;
-        for(int i = 0; i < len-1; ++i){
-            for(int j = i+1; j < len; ++j){
-                max_sub = max(max_sub, prices[j]-prices[i]);
+        for(int i = 0; i < len; ++i){
+            int sub = prices[i]-start;
+            if(prices[i] < start){
+                start = prices[i];
+            } else {
+                ans = max(ans, sub);
             }
         }
-        return max_sub;
+        return ans;
     }
 };
