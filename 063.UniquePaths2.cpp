4d69dc5c1da004754ce16e09fbc214eca21ffcb3
y498wu <49050856+y498wu@users.noreply.github.com>
2019-06-27 10:23:29 -0400

    Optimized Solution

--- 063.UniquePaths2.cpp
+++ 063.UniquePaths2.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        if(obstacleGrid[0][0] == 1) return 0;
-        obstacleGrid[0][0] = 1;
-        // iterate thru the first row
-        for(int i = 1; i < n; ++i){
-            if(obstacleGrid[0][i] == 1){
-                obstacleGrid[0][i] = 0;
-            } else {
-                obstacleGrid[0][i] = obstacleGrid[0][i-1];
-            }
-        }
-        // iterate thru the first column
-        for(int i = 1; i < m; ++i){
-            if(obstacleGrid[i][0] == 1){
-                obstacleGrid[i][0] = 0;
-            } else {
-                obstacleGrid[i][0] = obstacleGrid[i-1][0];
-            }
-        }
-        for(int i = 1; i < m; ++i){
-            for(int j = 1; j < n; ++j){
-                if(obstacleGrid[i][j] == 1 
-                   || obstacleGrid[i-1][j] > INT_MAX - obstacleGrid[i][j-1]){
-                    obstacleGrid[i][j] = 0;
-                } else {
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                }
-            }
-        }
-        return obstacleGrid[m-1][n-1];
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size() , n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][1] = 1;
+        for(int i = 1 ; i <= m ; ++i)
+            for(int j = 1 ; j <= n ; ++j)
+                if(dp[i-1][j] > INT_MAX - dp[i][j-1])
+                    dp[i][j] = 0;
+                else if(!obstacleGrid[i-1][j-1])
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+        return dp[m][n];
     }
 };
