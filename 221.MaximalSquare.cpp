4b4a7a8f2ae677e65e14e02e45848f439bd1b462
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-03 16:19:17 -0400

    55 / 68 test cases passed.

--- 221.MaximalSquare.cpp
+++ 221.MaximalSquare.cpp
@@ -7,7 +7,7 @@ public:
         int ans = 1;
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
-                if(matrix[i][j]){
+                if(matrix[i][j] == '1'){
                     ans = max(ans, tryhelper(i, j, m, n, matrix));
                 }
             }
@@ -17,23 +17,25 @@ public:
     
     int tryhelper(int i, int j, int m, int n, vector<vector<char>>& matrix){
         int k = 0;
-        while(matrix[i][j+k] && i+k < m-1 && j+k < n-1){
+        while(matrix[i][j+k] == '1' && i+k < m-1 && j+k < n-1){
             ++k;
         }
         --k;
         if(k == 0) return 1;
         int ans = 1;
+        //cout << "i is " << i << " and j is " << j;
+        //cout << " and k is " << k << endl;
         while(k >= 1){
             bool isallone = true;
             for(int a = 1; a <= k; ++a){
                 for(int b = 0; b <= k; ++b){
-                    if(matrix[i+a][j+b] == 0) isallone = false;
+                    if(matrix[i+a][j+b] == '0') isallone = false;
                 }
             }
             if(isallone == false){
                 --k;
             } else {
-                ans = k*k;
+                ans = (k+1)*(k+1);
                 break;
             }
         }
