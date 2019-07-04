290792a5511a3d81dbb82a2fbcc2ce1054fc5608
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-04 16:37:50 -0400

    dp solution

--- 221.MaximalSquare.cpp
+++ 221.MaximalSquare.cpp
@@ -4,42 +4,16 @@ public:
         int m = matrix.size();
         if(m == 0) return 0;
         int n = matrix[0].size();
-        int ans = 0;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(matrix[i][j] == '1'){
-                    ans = max(ans, tryhelper(i, j, m, n, matrix));
+        vector<vector<int>> ans(m+1, vector<int>(n+1, 0));
+        int anslen = 0;
+        for(int i = 1; i <= m; ++i){
+            for(int j = 1; j <= n; ++j){
+                if(matrix[i-1][j-1] == '1'){
+                    ans[i][j] = 1 + min(ans[i-1][j-1], min(ans[i][j-1], ans[i-1][j]));
+                    anslen = max(anslen, ans[i][j]);
                 }
             }
         }
-        return ans;
-    }
-    
-    int tryhelper(int i, int j, int m, int n, vector<vector<char>>& matrix){
-        int k = 0;
-        while(1){
-            if(matrix[i][j+k] != '1' || i+k >= m-1 || j+k >= n-1) break;
-            else ++k;
-        }
-        if(k == 0) return 1;
-        
-        int ans = 1;
-        while(k >= 1){
-            bool isallone = true;
-            for(int a = 0; a <= k; ++a){
-                for(int b = 0; b <= k; ++b){
-                    if(matrix[i+a][j+b] == '0') isallone = false;
-                }
-            }
-            if(isallone == false){
-                --k;
-            } else {
-                ans = (k+1)*(k+1);
-                break;
-            }
-        }
-        cout << "i is " << i << " and j is " << j;
-        cout << " and k is " << k << endl;
-        return ans;
+        return anslen*anslen;
     }
 };
