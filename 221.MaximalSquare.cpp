5639cd418c533db2246e67115badca39a6f0768d
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-04 15:34:12 -0400

    faster than 5.15%

--- 221.MaximalSquare.cpp
+++ 221.MaximalSquare.cpp
@@ -17,16 +17,16 @@ public:
     
     int tryhelper(int i, int j, int m, int n, vector<vector<char>>& matrix){
         int k = 0;
-        while(matrix[i][j+k] == '1' && i+k < m-1 && j+k < n-1){
-            ++k;
+        while(1){
+            if(matrix[i][j+k] != '1' || i+k >= m-1 || j+k >= n-1) break;
+            else ++k;
         }
         if(k == 0) return 1;
+        
         int ans = 1;
-        cout << "i is " << i << " and j is " << j;
-        cout << " and k is " << k << endl;
         while(k >= 1){
             bool isallone = true;
-            for(int a = 1; a <= k; ++a){
+            for(int a = 0; a <= k; ++a){
                 for(int b = 0; b <= k; ++b){
                     if(matrix[i+a][j+b] == '0') isallone = false;
                 }
@@ -38,6 +38,8 @@ public:
                 break;
             }
         }
+        cout << "i is " << i << " and j is " << j;
+        cout << " and k is " << k << endl;
         return ans;
     }
 };
