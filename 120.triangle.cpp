28ac0cf89ad6884ffb351f08787c37b23e81c794
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-18 18:37:08 -0400

    faster than 70.08%

--- 120.triangle.cpp
+++ 120.triangle.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
+        int m = triangle.size(), rowsize, lastsize = 1;
         if(m == 0 || triangle[0].size() == 0) return 0;
-        int minsofar = triangle[0][0];
-        int index = 0;
         for(int i = 1; i < m; ++i){
-            if(triangle[i][index] < triangle[i][index+1]){
-                minsofar += triangle[i][index];
-            } else {
-                minsofar += triangle[i][index+1];
-                index++;
+            triangle[i][0] += triangle[i-1][0];
+            rowsize = triangle[i].size();
+            triangle[i][rowsize-1] += triangle[i-1][lastsize-1];
+            lastsize = rowsize;
+            if(rowsize == 2) continue;
+            for(int j = 1; j < rowsize-1; ++j){
+                triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
             }
         }
-        return minsofar;
+        int smallest = INT_MAX;
+        for(int& item : triangle[m-1]) smallest = min(smallest, item);
+        return smallest;
     }
 };
