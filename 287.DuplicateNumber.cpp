d484b9b4bebbca4e0a01f2116892d45e12684990
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 14:08:33 -0400

    set method

--- 287.DuplicateNumber.cpp
+++ 287.DuplicateNumber.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size()-1; ++i){
-            if(nums[i] == nums[i+1]) return nums[i];
+        set<int> ans;
+        for(int i = 0; i < nums.size(); ++i){
+            if(ans.find(nums[i]) != ans.end()){
+                return nums[i];
+            } else {
+                ans.insert(nums[i]);
+            }
         }
         return 0;
     }
