56428d1c9c11e728bba4d01e6d161d6c2410fbb2
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 15:43:53 -0400

    simplified recursion

--- 104.MaximumDepthofBinaryTree.cpp
+++ 104.MaximumDepthofBinaryTree.cpp
@@ -10,15 +10,6 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == nullptr) return 0;
-        if(root->left == nullptr && root->right == nullptr){
-            return 1;
-        } else if (root->left == nullptr){
-            return maxDepth(root->right)+1;
-        } else if (root->right == nullptr){
-            return maxDepth(root->left)+1;
-        } else {
-            return max(maxDepth(root->right), maxDepth(root->left))+1;
-        }
+        return root ? max(maxDepth(root->left), maxDepth(root->right))+1 : 0;
     }
 };
