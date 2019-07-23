93ca8b6b62a18178d23b1ef49120732195785ab9
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 16:25:41 -0400

    unordered map solution

--- 242.ValidAnagram.cpp
+++ 242.ValidAnagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if(s.length() != t.length()) return false;
+        unordered_map<char, int> counts;
+        int slen = s.length();
+        for(int i = 0; i < slen; i++){
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for(auto count : counts)
+            if(count.second) return false;
+        return true;
     }
 };
