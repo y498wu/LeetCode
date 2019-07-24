0ee1f088e9aed59b770deb3d715d5f23c92e550c
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 22:25:14 -0400

    unordered_map solution

---
+++ 387.FirstUniqueChar.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> dict;
+        for(int i = 0; i < s.length(); i++){
+            dict[s[i]]++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(dict[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
