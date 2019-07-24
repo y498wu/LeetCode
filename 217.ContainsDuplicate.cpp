34c6e51f7225ffeb364ac14ef12ae1a47ce0d480
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 21:14:04 -0400

    sort solution

--- 217.ContainsDuplicate.cpp
+++ 217.ContainsDuplicate.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> checks;
-        for(auto num : nums){
-            if(checks.find(num) != checks.end()){
-                return true;
-            } else {
-                checks.insert(num);
-            }
+        if(nums.size() == 0) return false;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] == nums[i+1]) return true;
         }
         return false;
     }
