0290fa5190b4af14204223c0106f9c19735689a6
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-23 22:32:23 -0400

    simplify unordered map to 26 ints alphabet array

--- 387.FirstUniqueChar.cpp
+++ 387.FirstUniqueChar.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> dict;
+        int dict[26] = {0};
         for(int i = 0; i < s.length(); i++){
-            dict[s[i]]++;
+            dict[s[i]-'a']++;
         }
         for(int i = 0; i < s.length(); i++){
-            if(dict[s[i]] == 1) return i;
+            if(dict[s[i]-'a'] == 1) return i;
         }
         return -1;
     }
