5248f20ba37645bc627deb616ee4e4ea8c7cc4a7
y498wu <49050856+y498wu@users.noreply.github.com>
2019-07-29 11:27:57 -0400

    bucket sort (Learn something new!!)

--- 347.TopKFrequent.cpp
+++ 347.TopKFrequent.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
         vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i = 0; i < nums.size()-1; i++){
-            if(nums[i] != nums[i+1]){
-                k--;
-                ans.push_back(nums[i+1]);
+        if(!nums.size()) return ans;
+        unordered_map<int, int> dict;
+        for(auto num : nums)
+            dict[num]++;
+        vector<vector<int>> bucket(nums.size()+1);
+        for(auto kv : dict)
+            bucket[kv.second].push_back(kv.first);
+        for(int i = bucket.size()-1; i >= 0; i--){
+            for(int j = 0; j < bucket[i].size(); j++){
+                ans.push_back(bucket[i][j]);
+                if(ans.size() == k) return ans;
             }
-            if(k == 1) break;
         }
         return ans;
     }
